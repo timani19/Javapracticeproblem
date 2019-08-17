fb696e489da5b6a3326d367394d075049aa815e0
timani19 <tashfiaimani@gmail.com>
2019-08-17 19:58:03 -0400

    java practice

---
+++ helloWorld/Hello.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+/*public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hello Everyone"); // print will print all the statement in same line
+        System.out.print("!Welcome to the class");
+        System.out.println("Share your ID");
+        System.out.println("Name, DOB"); // println will print the statement in different lines
+
+    }
+} */
---
+++ homeworkone/MyLife.java
@@ -0,0 +1,40 @@
+package homeworkone;
+
+public class MyLife {
+    public void myName() {
+        String Name = "Tashfia Imani";
+        System.out.println("Name: " + Name);
+    }
+
+    public void myAge() {
+        int Age=25;
+        System.out.println("Age:" + Age);
+    }
+
+    public void myID() {
+        int ID = 3998;
+        System.out.println("ID:" + ID);
+    }
+
+    public void emailAddress() {
+        String Email = "tashfiaimani@gmail.com";
+        System.out.println("Email:" + Email);
+    }
+
+    public void myCity() {
+        String City = "Richmond Hill";
+        System.out.println("City:" + City);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
